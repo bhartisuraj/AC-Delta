ec41cbdc55dd9b05e30071679e5183303ef2d50c
SurajKumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2023-08-17 15:16:49 +0000

    js_part07

--- 07_javascriptpart07.js
+++ 07_javascriptpart07.js
@@ -6,7 +6,13 @@ const student = {
     age: 23,
     eng: 95,
     math: 93,
-    phy: 97
+    phy: 97,
+    getAvg() {
+      let avg = (this.eng + this.math + this.phy) / 3;
+      console.log(avg);
+    }
   }
   
   console.log(student.name);// To write value of some key(object name.key)
+  console.log(student.getAvg());// When we call it, it will throw error eng is not defined because it can't access value above getAvg's(){ } scope. If we write  this.eng it means we have defined value of eng in same object but not in same scope.
+  

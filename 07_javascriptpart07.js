dbcf4daa78782673825ff8226f018358ef20e49c
SurajKumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2023-08-17 14:55:19 +0000

    js_part07

---
+++ 07_javascriptpart07.js
@@ -0,0 +1,12 @@
+/************************ JavaScript part07 ************************/
+
+/************************ this keyword in JavaScript:- This keyword refers to an object that is executing the current piece of code. ************************/
+const student = {
+    name: "shradha",
+    age: 23,
+    eng: 95,
+    math: 93,
+    phy: 97
+  }
+  
+  console.log(student.name);// To write value of some key(object name.key)
